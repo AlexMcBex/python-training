02ba3350a07dcd2f7f099971049de7146502f77e
alexmcbex <alessiobelli94@gmail.com>
2024-09-30 12:47:08 -0400

    finalized pypacpoe

--- Py-Pac-Poe.py
+++ Py-Pac-Poe.py
@@ -1,7 +1,3 @@
-print(" ----------------------\n Let's play Py-Pac-Poe!\n ----------------------")
-
-
-
 status = {
     'board':{
     'a1': ' ',
@@ -14,7 +10,6 @@ status = {
     'b3': ' ',
     'c3': ' '
     },
-    'turn': True,
     'x_moves': [],
     'o_moves': [],
     'moves': ['a1','b1','c1','a2','b2','c2','a3','b3','c3'],
@@ -27,7 +22,9 @@ status = {
     ['c1', 'c2', 'c3'],
     ['a1', 'b2', 'c3'],
     ['c1', 'b2', 'a3']
-    ]
+    ],
+    'x_score': 0,
+    'o_score': 0,
 }
 
 display_board = '     a   b   c\n1)   {} | {} | {}   \n   -------------\n2)   {} | {} | {}   \n   -------------\n3)   {} | {} | {}   '.format(
@@ -42,14 +39,7 @@ display_board = '     a   b   c\n1)   {} | {} | {}   \n   -------------\n2)   {}
     status['board']['c3']
 )
 
-
-print(display_board)
-
-
-def play_again():
-    ask_rematch = input('Play again? (y / n): ')
-    if ask_rematch == 'y':
-        status['turn'] = True
+def reset_game():
         status['x_moves'] = []
         status['o_moves'] = []
         status['moves'] = ['a1','b1','c1','a2','b2','c2','a3','b3','c3']
@@ -64,78 +54,65 @@ def play_again():
             'b3': ' ',
             'c3': ' '
         }
+
+def play_again(turn):
+    ask_rematch = input('Play again? (y / n): ')
+    if ask_rematch == 'y':
+        reset_game()
         print(display_board)
-        move()
+        move(turn)
     else:
+        print('Thank you for playing!')
         exit()
+
+def display_score():
+    print('X: {}   -   O: {}'.format(status['x_score'], status['o_score']))
 
 
 def check_win(turn):
-    if turn == 'x':
-        for sublist in status['winning_combos']:
-            if any(set(sublist).issubset(status['x_moves']) for sublist in status['winning_combos']):
-                print('X PLAYER WINS')
-                play_again()
+    for sublist in status['winning_combos']:
+        if any(set(sublist).issubset(status['{}_moves'.format(turn)]) for sublist in status['winning_combos']):
+            print('{} PLAYER WINS'.format(turn.capitalize()))
+            status['{}_score'.format(turn)] += 1
+            display_score()
+            if turn == 'x':
+                play_again('o')
             else:
-                move()
-    else:
-        for sublist in status['winning_combos']:
-            if any(set(sublist).issubset(status['o_moves']) for sublist in status['winning_combos']):
-                print('O PLAYER WINS')
-                play_again()
+                play_again('x')
+        else:
+            if turn == 'x':
+                move('o')
             else:
-                move()
+                move('x')
 
-def move():
+def move(turn):
     if len(status['moves']) == 0:
+        display_score()
         print('TIE GAME')
-        play_again()
-    if status['turn']:
-        player_move = input('Player X move: ')
-        if player_move in status['moves']:
-            status['x_moves'].append(player_move)
-            status['moves'].pop(status['moves'].index(player_move))
-            status['board'][player_move] = 'X'
-            status['turn'] = False
-            display_board = '     a   b   c\n1)   {} | {} | {}   \n   -------------\n2)   {} | {} | {}   \n   -------------\n3)   {} | {} | {}   '.format(
-                status['board']['a1'],
-                status['board']['b1'],
-                status['board']['c1'],
-                status['board']['a2'],
-                status['board']['b2'],
-                status['board']['c2'],
-                status['board']['a3'],
-                status['board']['b3'],
-                status['board']['c3']
-            )
-            print(display_board)
-            check_win('x')
-        else:
-            print('error, move not avaiable, try again')
-            move()
+        play_again('x')
+    player_move = input('Player {} move: '.format(turn.capitalize()))
+    if player_move in status['moves']:
+        status['{}_moves'.format(turn)].append(player_move)
+        status['moves'].pop(status['moves'].index(player_move))
+        status['board'][player_move] = turn.capitalize()
+        status['turn'] = False
+        display_board = '     a   b   c\n1)   {} | {} | {}   \n   -------------\n2)   {} | {} | {}   \n   -------------\n3)   {} | {} | {}   '.format(
+            status['board']['a1'],
+            status['board']['b1'],
+            status['board']['c1'],
+            status['board']['a2'],
+            status['board']['b2'],
+            status['board']['c2'],
+            status['board']['a3'],
+            status['board']['b3'],
+            status['board']['c3']
+        )
+        print(display_board)
+        check_win(turn)
     else:
-        player_move = input('Player O move: ')
-        if player_move in status['moves']:
-            status['o_moves'].append(player_move)
-            status['moves'].pop(status['moves'].index(player_move))
-            status['board'][player_move] = 'O'
-            status['turn'] = True
-            display_board = '     a   b   c\n1)   {} | {} | {}   \n   -------------\n2)   {} | {} | {}   \n   -------------\n3)   {} | {} | {}   '.format(
-                status['board']['a1'],
-                status['board']['b1'],
-                status['board']['c1'],
-                status['board']['a2'],
-                status['board']['b2'],
-                status['board']['c2'],
-                status['board']['a3'],
-                status['board']['b3'],
-                status['board']['c3']
-            )
-            print(display_board)
-            check_win('o')
-        else:
-            print('error, move not avaiable, try again')
-            move()
+        print('error, move not avaiable, try again')
+        move(turn)
 
-
-move()
+print(" ----------------------\n Let's play Py-Pac-Poe!\n ----------------------")
+print(display_board)
+move('x')
